b89577048264070fd137173d07802b917aa97828
TothLorand98 <49196738+TothLorand98@users.noreply.github.com>
2019-10-28 23:14:09 +0200

    Update Carte.java

--- Carte.java
+++ Carte.java
@@ -68,23 +68,60 @@ public class Carte {
 	
 	public static void main(String[] args)
 	{
-		Carte noapteBuna = new Carte("Noapte buna, copii!");
-		Autor rpGheo = new Autor("Radu Pavel Gheo");
-		noapteBuna.AddAutor(rpGheo);
-		Section cap1 = new Section("Capitolul 1");
-		Section cap11 = new Section("Capitolul 1.1");
-		Section cap111 = new Section("Capitolul 1.1.1");
-		Section cap1111 = new Section("Subchapter 1.1.1.1");
-		noapteBuna.addContent(new Paragraf("Multumesc celor care au facut posibila......"));
-		noapteBuna.addContent(cap1);
-		cap1.addElm(new Paragraf("Moto capitol"));
-		cap1.addElm(cap11);
-		cap11.addElm(new Paragraf("Text from subchapter 1.1"));
-		cap11.addElm(cap111);
-		cap111.addElm(new Paragraf("Text from subchapter 1.1.1"));
-		cap111.addElm(cap1111);
-		cap1111.addElm(new Imagine("Image subchapter 1.1.1.1"));
-		noapteBuna.print();
+		/*long startTime = System.currentTimeMillis();
+		Imagine img1 = new Imagine("Pamela Anderson");
+		Imagine img2 = new Imagine("Kim Kardashian");
+		Imagine img3 = new Imagine("Kirby Griffin");
+		Section playboyS1 = new Section("Front Cover");
+		playboyS1.addElm(img1);
+		Section playboyS2 = new Section("Summer Girls");
+		playboyS2.addElm(img2);
+		playboyS2.addElm(img3);
+	    Carte playboy = new Carte("Playboy");
+
+		playboy.addContent(playboyS1);
+		playboy.addContent(playboyS2);
+		long endTime = System.currentTimeMillis();
+		System.out.println("Creation of the content took " + (endTime -
+		startTime) + " milliseconds");
+		startTime = System.currentTimeMillis();
+		playboyS1.print();
+		endTime = System.currentTimeMillis();
+		System.out.println("Printing of the section 1 took " + (endTime -
+		startTime) + " milliseconds");
+		startTime = System.currentTimeMillis();
+		playboyS1.print();
+		endTime = System.currentTimeMillis();
+		System.out.println("Printing again the section 1 took " + (endTime -
+		startTime) + " milliseconds");*/
+		
+		
+		
+			long startTime = System.currentTimeMillis();
+			ImageProxy img1 = new ImageProxy("Pamela Anderson");
+			ImageProxy img2 = new ImageProxy("Kim Kardashian");
+			ImageProxy img3 = new ImageProxy("Kirby Griffin");
+			Section playboyS1 = new Section("Front Cover");
+			playboyS1.addElm(img1);
+			Section playboyS2 = new Section("Summer Girls");
+			playboyS2.addElm(img2);
+			playboyS2.addElm(img3);
+		    Carte playboy = new Carte("Playboy");
+			playboy.addContent(playboyS1);
+			playboy.addContent(playboyS2);
+			long endTime = System.currentTimeMillis();
+			System.out.println("Creation of the content took " + (endTime -
+			startTime) + " milliseconds");
+			startTime = System.currentTimeMillis();
+			playboyS1.print();
+			endTime = System.currentTimeMillis();
+			System.out.println("Printing of the section 1 took " + (endTime -
+			startTime) + " milliseconds");
+			startTime = System.currentTimeMillis();
+			playboyS1.print();
+			endTime = System.currentTimeMillis();
+			System.out.println("Printing again the section 1 took " + (endTime -
+			startTime) + " milliseconds");
 	}
 	
 }
