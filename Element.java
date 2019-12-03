2f0dd827a5de9d4e7814e9f539d05e4531eb2c0e
TothLorand98 <49196738+TothLorand98@users.noreply.github.com>
2019-12-03 21:53:31 +0200

    Update Element.java

--- Element.java
+++ Element.java
@@ -1,6 +1,17 @@
 package dp_lab2;
 
 public interface Element {
+	
+
+	
 	public void print();
 	public void accept(Visitor a);
+
+	public void add(Element element);
+
+  public void remove(Element element);
+
+  public Element getElement(int index);
+  public void setNewValue(String newValue);
+  
 }
