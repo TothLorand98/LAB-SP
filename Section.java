c51de845b920facebc45114867b05ff36d0b829c
TothLorand98 <49196738+TothLorand98@users.noreply.github.com>
2019-12-03 21:54:37 +0200

    Update Section.java

--- Section.java
+++ Section.java
@@ -2,10 +2,14 @@ package dp_lab2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observer;
 
-public class Section implements Element{
+public class Section implements Element,Observable{
 	private String title;
 	private List<Element> content;
+	private String oldValue;
+	private List<Observerr>obs=new ArrayList<Observerr>();
+
 
 	public Section(String title) {
 		super();
@@ -37,12 +41,54 @@ public class Section implements Element{
 			x.print();
 		}
 	}
-	public void accept(Visitor a) {
+	
+public void accept(Visitor a) {
 	a.visit(this);
 	
 	 for (Element x : content) {
 	      x.accept(a);
 	    }
 		
+}
+
+@Override
+public void add(Element element) {
+	// T
+	
+}
+
+@Override
+public void remove(Element element) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void addObserver(Observerr obs) {
+	this.obs.add(obs);
+	
+}
+
+@Override
+public void removeObserver(Observerr obs) {
+	this.obs.remove(obs);
+	
+}
+
+@Override
+public void notifyObservers() {
+	for(Observerr o:obs)
+	{
+		o.update(oldValue,title);
+	}
+	
+}
+
+@Override
+public void setNewValue(String newValue) {
+	oldValue=this.title;
+    title=newValue;
+	this.notifyObservers();
+	
 }
 }
