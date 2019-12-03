a225dbccc50282648603fd41b2685300b061b3fe
TothLorand98 <49196738+TothLorand98@users.noreply.github.com>
2019-12-03 21:57:18 +0200

    Update ImageProxy.java

--- ImageProxy.java
+++ ImageProxy.java
@@ -1,10 +1,14 @@
 package dp_lab2;
 
+import java.util.List;
+import java.util.Observer;
 import java.util.concurrent.TimeUnit;
 
-public class ImageProxy implements Element {
+public class ImageProxy implements Element,Observable {
 	private String nume;
+	private String oldValue;
 	private Imagine realImage;
+	private List<Observerr> obs;
 
 	public ImageProxy(String nume) {
 		super();
@@ -20,7 +24,50 @@ public class ImageProxy implements Element {
 		realImage.print();
 	
 	}
-	public void accept(Visitor a) {
+	
+public void accept(Visitor a) {
 		a.visit(this);
 	}
+
+	@Override
+  public void add(Element element) {
+    System.out.println("ERORR - IMG PROXY");
+  }
+
+  @Override
+  public void remove(Element element) {
+    System.out.println("ERORR - IMG PROXY");
+  }
+
+  @Override
+  public Element getElement(int index) {
+    System.out.println("ERORR - IMG PROXY");
+    return null;
+  }
+@Override
+public void addObserver(Observerr obs) {
+	this.obs.add( obs);
+	
+}
+@Override
+public void removeObserver(Observerr obs) {
+	 this.obs.remove(obs);
+	
+}
+@Override
+public void notifyObservers() {
+	for(Observerr o :obs)
+	{
+		o.update(oldValue,nume);
+	}
+	
+}
+@Override
+public void setNewValue(String newValue) {
+	oldValue=this.nume;
+    nume=newValue;
+    this.notifyObservers();
+	
+}
+
 }
